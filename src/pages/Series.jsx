340e2079e80ae8bf9f2c032b2e0d372c35ec43d7
furkanavc1 <furkanavci039@gmail.com>
2023-04-05 23:21:32 +0300

    dropdown style added

--- src/pages/Series.jsx
+++ src/pages/Series.jsx
@@ -1,15 +1,44 @@
 import React from "react";
 import Sample from "../assets/data/sample.json";
 import { useState } from "react";
-export const Series = () => {
-  const [filteredList, setFilteredList] = useState("");
+import { Icon } from '@iconify/react';
 
+export const Series = () => {
+  const selectBox = [
+    {
+      value: "1",
+      desc: "Yeniye Göre Sırala",
+    },
+    {
+      value: "2",
+      desc: "Eskiye Göre Sırala",
+    },
+    {
+      value: "3",
+      desc: "Puana Göre Sırala",
+    },
+    {
+      value: "4",
+      desc: "Rastgele Sırala",
+    },
+  ];
+
+  const [filteredList, setFilteredList] = useState("");
+  const [checkBoxVal, setCheckBoxVal] = useState("");
+  const [toggleDropdown, setToggleDropdown] = useState(false);
+
+  const handleToggle = () => {
+    setToggleDropdown((current) => !current);
+  };
   const filterSeries = (e) => {
     if (e.target.value.length >= 3) {
       setFilteredList(e.target.value);
     } else if (e.target.value.length === 0) {
       setFilteredList(e.target.value);
     }
+  };
+  const compare = (a, b) => {
+    return a.releaseYear - b.releaseYear;
   };
   return (
     <div className="container  my-10 px-2">
@@ -21,14 +50,36 @@ export const Series = () => {
           className="border border-slate-500 rounded-2xl w-full md:w-1/3 py-5 px-4 text-black"
           placeholder="Film ara"
         />
+
+        <div className="flex items-center w-full lg:w-1/5 relative">
+          <div
+            onClick={handleToggle}
+            className=" border border-slate-500 p-5 rounded-2xl w-full z-20 bg-white flex justify-between"
+          >
+            <span className="text-black">
+            Sıralama
+            </span>
+            <Icon icon="carbon:chevron-down" height={24} />
+
+          </div>
+          {toggleDropdown === false && (
+            <div className="flex flex-col divide-y pt-7 absolute top-10 left-0  border border-slate-500 rounded-2xl w-full bg-white z-10">
+              {selectBox.map((e,i) => (
+                <span key={i} className="text-black px-5 py-2">{e.desc}</span>
+              ))}
+            </div>
+          )}
+        </div>
       </div>
       <div className="grid grid-cols-1 md:grid-cols-4 lg:grid-cols-6 gap-4">
         {Sample.entries
+          .filter((i) => i.programType === "series")
           .filter((i) => {
             return filteredList.toLowerCase() === ""
               ? i
               : i.title.toLowerCase().includes(filteredList);
           })
+          .sort(compare)
           .map((e) => (
             <div
               to={e.path}
