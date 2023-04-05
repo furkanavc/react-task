1620b40dce4815ca350cc1446b9fb5cdc6ade449
furkanavc1 <furkanavci039@gmail.com>
2023-04-05 18:46:22 +0300

    movies added dynamiclly

--- src/pages/Films.jsx
+++ src/pages/Films.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import Sample from '../assets/data/sample.json'
 
 export const Films = () => {
     
   return (
-    <div>Films</div>
+    <div className="container grid grid-cols-1 md:grid-cols-4 lg:grid-cols-6 gap-4 my-10">
+    {Sample.entries.filter(i=>i.programType === 'movie').map((e) => (
+      <div
+        to={e.path}
+        key={e.id}
+        className="flex flex-col space-y-4 items-center w-full"
+      >
+        <div className="rounded-lg border border-black h-56 w-56 md:w-full">
+          <img
+            src={e.images['Poster Art'].url}
+            alt={e.title}
+            className="w-full h-full object-contain lg:object-cover"
+          />
+        </div>
+        <span className="text-black">{e.title}</span>
+      </div>
+    ))}
+  </div>
   )
 }
