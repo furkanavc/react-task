7fd725297f12ef9cd1b4de505c4737917211d2d6
furkanavc1 <furkanavci039@gmail.com>
2023-04-05 18:21:51 +0300

    home links added

---
+++ src/assets/images/index.js
@@ -0,0 +1,4 @@
+import FilmCoverImage from './film-cover.png'
+import SerieCoverImage from './serie-cover.png'
+
+export  {FilmCoverImage, SerieCoverImage}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,20 +1,46 @@
-import React from 'react'
-import {  Link } from "react-router-dom";
-
+import React from "react";
+import { Link } from "react-router-dom";
+import {FilmCoverImage,SerieCoverImage} from '../assets/images'
 export const Home = () => {
+  const popularTitles = [
+    {
+      id: 1,
+      name: "Film",
+      path: "/films",
+      img: FilmCoverImage
+    },
+    {
+      id: 2,
+      name: "Dizi",
+      path: "/series",
+      img: SerieCoverImage,
+    },
+  ];
   return (
-    <nav>
-        <ul className='flex space'>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/series">Series</Link>
-          </li>
-          <li>
-            <Link to="/films">Films</Link>
-          </li>
-        </ul>
-      </nav>
-  )
-}
+    <div>
+      <div className="bg-black/80 px-2 sm:px-0">
+        <div className="container py-5">
+          <span className="text-xl">Popüler Başlıklar</span>
+        </div>
+      </div>
+      <div className="container grid grid-cols-1 md:grid-cols-4 lg:grid-cols-6 gap-4 my-10">
+        {popularTitles.map((e) => (
+          <Link
+            to={e.path}
+            key={e.id}
+            className="flex flex-col space-y-4 items-center w-full"
+          >
+            <div className="rounded-lg border border-black h-56 w-56 md:w-full">
+              <img
+                src={e.img}
+                alt={e.name}
+                className="w-full h-full object-contain lg:object-cover"
+              />
+            </div>
+            <span className="text-black">{e.name}</span>
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
+};
