af6b948e73bc00ba8947be34530f0dcf87ecc9b9
furkanavc1 <furkanavci039@gmail.com>
2023-04-05 17:32:25 +0300

    footer added

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,7 +1,26 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 export const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer className="bg-[#1A1A1A] space-y-5 py-5 ">
+      <div className="container space-y-4 flex items-center flex-col  md:items-start ">
+        <div className=" flex items-center divide-x">
+          <Link className="pr-3 text-sm sm:text-base" to="/">
+            Anasayfa
+          </Link>
+          <Link className="px-3 text-sm sm:text-base" to="/">
+            Kullanıcı Sözleşmesi
+          </Link>
+          <Link className="pl-3 text-sm sm:text-base" to="/">
+            Gizlilik Sözleşmesi
+          </Link>
+        </div>
+        <div className="flex space-x-3">
+          <div className="h-5 w-5 bg-red-500 rounded-full"></div>
+          <div className="h-5 w-5 bg-red-500 rounded-full"></div>
+        </div>
+      </div>
+    </footer>
+  );
+};
--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,16 +1,15 @@
-import React from 'react'
+import React from "react";
 
 export const Header = () => {
   return (
-    <header className='bg-black py-3 px-2 sm:px-0'>
-      <div className='flex justify-between w-full container items-center'>
-
-     <span className='text-2xl'>Bulut MD</span>
-     <div className='flex space-x-4 '>
-      <button className='text-xl hidden md:block'>Giriş</button>
-      <button className='bg-[#EF4B24] px-5 py-2'>Deneme Başlat</button>
-     </div>
+    <header className="bg-black py-3 px-2 sm:px-0">
+      <div className="flex justify-between w-full container items-center">
+        <span className="text-2xl">Bulut MD</span>
+        <div className="flex space-x-4 ">
+          <button className="text-xl hidden md:block">Giriş</button>
+          <button className="bg-[#EF4B24] px-5 py-2">Deneme Başlat</button>
+        </div>
       </div>
     </header>
-  )
-}
+  );
+};
