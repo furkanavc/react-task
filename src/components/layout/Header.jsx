f2297222ff96ebda831e98d151f4ac8de12f4ae8
furkanavc1 <furkanavci039@gmail.com>
2023-04-05 17:14:59 +0300

    header comp

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
-import {  Link } from "react-router-dom";
 
 export const Header = () => {
   return (
-    <header className='container'>
-     <span>Bulut MD</span>
+    <header className='bg-black py-3 px-2 sm:px-0'>
+      <div className='flex justify-between w-full container items-center'>
+
+     <span className='text-2xl'>Bulut MD</span>
+     <div className='flex space-x-4 '>
+      <button className='text-xl hidden md:block'>Giriş</button>
+      <button className='bg-[#EF4B24] px-5 py-2'>Deneme Başlat</button>
+     </div>
+      </div>
     </header>
   )
 }
