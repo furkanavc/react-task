a70391582a35f6c5d8d4e9597d83dcc0ef684746
furkanavc1 <furkanavci039@gmail.com>
2023-04-06 13:16:12 +0300

    mobile header padding added

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 export const Header = () => {
   return (
-    <header className="bg-black py-3 h-20 flex items-center sm:px-0 text-white">
+    <header className="bg-black py-3 h-20 flex items-center px-2 sm:px-0 text-white">
       <div className="flex justify-between w-full container items-center">
         <Link className="text-2xl" to="/">Bulut MD</Link>
         <div className="flex space-x-4 ">
